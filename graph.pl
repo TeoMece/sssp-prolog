cc642aa81b3e2c2bc4cb1333f79890c979cf975d
TeoMece <teomece@gmail.com>
2024-02-15 13:20:07 +0100

    Heap e Graph commentati e funzinanti

--- graph.pl
+++ graph.pl
@@ -5,14 +5,17 @@
 %%% new_graph/1 Inizializzazione del grafo vuoto
 
 new_graph(G) :- 
+    atom(G),
     graph(G),
     !.
 new_graph(G) :- 
+    atom(G),
     assert(graph(G)),
     !.
 
 %%% delete_graph/1 Eliminazione del grafo
 delete_graph(G) :- 
+    atom(G),
     retract(graph(G)),
     retractall(vertex(G, _)),
     retractall(edge(G, _, _, _)),
@@ -21,21 +24,29 @@ delete_graph(G) :-
 %%% new_vertex/2 Aggiunta di un vertice al grafo
 
 new_vertex(G, V) :- 
+    atom(G),
+    atom(V),
+    graph(G),
     vertex(G, V),
     !.
 new_vertex(G, V) :- 
+    atom(G),
+    atom(V),
     graph(G),
     assert(vertex(G, V)),
     !.
 
 %% vertices /2 Restituisce la lista dei vertici del grafo 
 vertices(G, Vs) :- 
+    atom(G),
+    atom(Vs),
     graph(G),
     findall(vertex(G,V), vertex(G, V), Vs),
     !.
 
 %% list_vertices /1 Stampa la lista dei vertici del grafo
 list_vertices(G) :-
+    atom(G),
     listing(vertex(G, _)),
     !.
 
@@ -43,48 +54,77 @@ list_vertices(G) :-
 new_edge(G, V1, V2) :- 
     new_edge(G, V1, V2, 1),
     !.
-new_edge(G, vertex(G, V1), vertex(G, V2), W) :- 
-    edge(G, vertex(G,V1), vertex(G,V2), W),
-    !.
-new_edge(G, V1, V2, W) :- 
+
+new_edge(G, vertex(G, V1), vertex(G, V2), W) :- %% Se esiste non faccio niente
     edge(G, vertex(G,V1), vertex(G,V2), W),
     !.
 new_edge(G, vertex(G, V1), vertex(G, V2), W) :-
     graph(G),
     vertex(G, V1),
     vertex(G, V2),
+    integer(W),
+    W >= 0,
+    non_existing_edge(G, V1, V2),
     assert(edge(G, vertex(G, V1), vertex(G, V2), W)),
     !.
-new_edge(G, V1, V2, W) :-
-    new_edge(G, vertex(G, V1), vertex(G, V2), W),
+
+new_edge(G, V1, V2, W) :- %% Se esiste non faccio niente
+    edge(G, vertex(G,V1), vertex(G,V2), W),
     !.
 
+new_edge(G, V1, V2, W) :- 
+    vertex(G, V1),
+    vertex(G, V2),
+    graph(G),
+    integer(W),
+    W >= 0,
+    non_existing_edge(G, V1, V2),
+    assert(edge(G, vertex(G, V1), vertex(G, V2), W)),
+    !.
+
+%% not_existing_edge/3 controllo se esiste un arco tra due vertici
+non_existing_edge(G, V1, V2) :- 
+    graph(G),
+    \+ edge(G, vertex(G, V1), vertex(G, V2), _).
 
 %% edges /2 Restituisce la lista degli archi del grafo
 edges(G, Es) :- 
+    atom(G),
+    var(Es),
     graph(G),
     findall(edge(G, V1, V2, W), edge(G, V1, V2, W), Es),
     !.
 
-%% neighbors /3 Restituisce la lista dei vicini di un vertice
+%% neighbors /3 Restituisce la lista dei vicini di un vertice //TODO codice ripetuto
 neighbors(G, vertex(G,V), Ns) :- 
+    atom(G),
+    atom(V),
+    var(Ns),
     graph(G),
     findall(edge(G, vertex(G,V), N, W), edge(G, vertex(G,V), N, W), ArchiUscenti),
     findall(edge(G, N, vertex(G,V), W), edge(G, N, vertex(G,V), W), ArchiEntranti),
     append(ArchiUscenti, ArchiEntranti, Ns),
     !.
 neighbors(G, V, Ns) :- 
-    neighbors(G, vertex(G,V), Ns),
+    atom(G),
+    atom(V),
+    var(Ns),
+    graph(G),
+    findall(edge(G, vertex(G,V), N, W), edge(G, vertex(G,V), N, W), ArchiUscenti),
+    findall(edge(G, N, vertex(G,V), W), edge(G, N, vertex(G,V), W), ArchiEntranti),
+    append(ArchiUscenti, ArchiEntranti, Ns),
     !.
 
 %%% list_edges /1 Stampa la lista degli archi del grafo
 list_edges(G) :- 
+    atom(G),
     graph(G),
     listing(edge(G, _, _, _)),
     !.
 
 %%% list_graph /1 Stampa il grafo
 list_graph(G) :- 
+    atom(G),
     graph(G),
     list_vertices(G),
     list_edges(G),
--- minheap.pl
+++ minheap.pl
@@ -3,17 +3,21 @@
 
 %% new_heap/1 - Crea un nuovo heap
 new_heap(H) :- 
+    atom(H),
     heap(H, _), 
     !.
 new_heap(H) :- 
+    atom(H),
     assert(heap(H, 0)), 
     !.
 
 %% heap_size/2 - Restituisce la dimensione dell'heap
 heap_size(H, S) :- 
+    atom(H),
     heap(H, S).
 %% delete_heap/1 - Elimina un heap
 delete_heap(H) :-
+    atom(H),
     heap(H, _),
     !,
     retractall(heap(H, _)),
@@ -21,16 +25,20 @@ delete_heap(H) :-
 
 %% empty/1 - Verifica se l'heap è vuoto
 empty(H) :-
+    atom(H),
     heap(H, 0).
 %% not_empty/1 - Verifica se l'heap non è vuoto
 not_empty(H) :-
+    atom(H),
     heap(H, S),
     S > 0.
 %% head/3 - Restituisce la testa dell'heap
 head(H, K, V) :-
+    heap(H, _),
     heap_entry(H, 1, K, V).
 %% insert/3 - Inserisce un elemento nell'heap
 insert(H, K, V) :-
+    heap(H, _),
     is_key_not_used(H, K),
     heap_size(H, S),
     NewS is S + 1,
@@ -41,34 +49,42 @@ insert(H, K, V) :-
 %% extract/3 - Estrae un elemento dall'heap
 extract(H, K, V) :-
     head(H, K, V),
-    retract(heap_entry(H, K, V)),
-    decrease_position(H),
+    retract(heap_entry(H, 1, K, V)),
+    shift_left(H),
     heap_size(H, S),
-    sort_heap(H, 1, S).
-%% decrease_position/1 - Decrementa la posizione di tutti gli elementi dell'heap
-decrease_position(H) :-
+    NewS is S - 1,
+    retract(heap(H, S)),
+    assert(heap(H, NewS)).
+
+%% shift_left/1 - Decrementa la posizione di tutti gli elementi dell'heap
+shift_left(H) :-
     heap_size(H, S),
-    decrease_position(H, S).
+    shift_left(H, S).
 
-decrease_position(H, 0) :- !.
+shift_left(_, 1) :- !.
 
-decrease_position(H, P) :-
+shift_left(H, P) :-
     NewP is P - 1,
-    decrease_position(H, NewP).
-    modify_key(H, P, NewP, _).
+    shift_left(H, NewP),
+    retract(heap_entry(H, P, K, V)),
+    assert(heap_entry(H, NewP, K, V)).
 %% modify_key/3 - Modifica la chiave di un elemento dell'heap
 modify_key(H, NewKey, OldKey, V):-
+    heap_size(H, S),
     is_key_not_used(H, NewKey),
     heap_entry(H, P, OldKey, V),
     retract(heap_entry(H, P, OldKey, V)),
     assert(heap_entry(H, P, NewKey, V)),
-    heap_size(H, S),
     sort_heap(H, 1, S).
 %% modify_value/3 - Modifica il valore di un elemento dell'heap
 list_heap(H) :-
+    heap(H, _),
     listing(heap_entry(H, _, _, _)).
 %% modify_value/3 - Modifica il valore di un elemento dell'heap
 swap(H, P1, P2) :-
+    heap(H, _),
+    integer(P1),
+    integer(P2),
     P1 = P2,
     !.
 %% modify_value/3 - Modifica il valore di un elemento dell'heap
@@ -81,6 +97,9 @@ swap(H, P1, P2) :-
     assert(heap_entry(H, P2, K1, V1)).
 %% sort_heap/3 - Ordina l'heap
 sort_heap(H, S, E) :-
+    heap(H, _),
+    integer(S),
+    integer(E),
     S = E,
     !.
 sort_heap(H, S, E) :-
@@ -92,6 +111,9 @@ sort_heap(H, S, E) :-
     !.
 %% min_key/4 - Restituisce la chiave minima dell'heap
 min_key(H, S, E, M) :-
+    heap(H, _),
+    integer(S),
+    integer(E),
     S = E,
     !,
     heap_entry(H, E, M, _).
